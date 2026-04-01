c72b553fe477907318fc40a5ba57d5a6bd6d353c
jin605 <jinddd3@gmail.com>
2026-04-01 16:29:33 +0900

    feat : js 기본 문법

---
+++ code/boj1697.java
@@ -0,0 +1,23 @@
+package code;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj1697 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] location = new int[100001];
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+
+        
+    }
+    
+}
